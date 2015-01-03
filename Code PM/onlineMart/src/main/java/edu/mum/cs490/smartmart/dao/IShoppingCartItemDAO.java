123b9c2bd2f6eeb71972698d9fb034affa8cb53a
seadaghish <seadaghish@cs_labv32-202.cs.mum.edu>
2015-01-03 11:31:25 -0600

    created DAO Interfaces

---
+++ Code PM/onlineMart/src/main/java/edu/mum/cs490/smartmart/dao/IShoppingCartItemDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.mum.cs490.smartmart.dao;
+
+import edu.mum.cs490.smartmart.domain.Customer;
+import edu.mum.cs490.smartmart.domain.ShoppingCartItem;
+import java.util.List;
+
+/**
+ *
+ * @author Senai
+ */
+public interface IShoppingCartItemDAO   extends IGenericDAO<ShoppingCartItem, Long>{
+    
+   
+     
+}
